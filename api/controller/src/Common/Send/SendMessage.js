ea2290b774f021dfffcb800004fa4e858b4248ac
ryosuke-220 <ryoske0220@gmail.com>
2025-02-21 21:12:07 +0900

    Update SendMessage.js

--- api/controller/src/Common/Send/SendMessage.js
+++ api/controller/src/Common/Send/SendMessage.js
@@ -1,6 +1,66 @@
-const FlexMessageJson = require('../Template/FlexMessage.json');
-const ButtonMessageJson = require('../Template/ButtonMessage.json');
+const fs = require('fs');
+const csv = require('csv-parser');
+
 const ErrorMessageJson = require('../Template/ErrorMessage.json');
+
+// CSVファイルのパス
+const csvFilePath = 'path/to/your/csvfile.csv';
+
+// CSVを読み込んでFlexメッセージを生成
+function generateFlexMessage() {
+  const buttons = [];
+
+  fs.createReadStream(csvFilePath)
+    .pipe(csv())
+    .on('data', (row) => {
+      buttons.push({
+        type: 'text',
+        text: row['Button Text'],
+        action: {
+          type: 'message',
+          label: row['Button Text'],
+          text: row['Response Text'],
+        },
+        color: '#42659a',
+        margin: '10px',
+      });
+    })
+    .on('end', () => {
+      const flexMessage = {
+        type: 'flex',
+        altText: 'カフェ利用案内',
+        contents: {
+          type: 'bubble',
+          body: {
+            type: 'box',
+            layout: 'vertical',
+            contents: [
+              {
+                type: 'text',
+                text: 'カフェ利用案内',
+                weight: 'bold',
+                size: 'xl',
+              },
+              {
+                type: 'text',
+                text: '知りたいことをタップしてください。',
+                wrap: true,
+              },
+            ],
+          },
+          footer: {
+            type: 'box',
+            layout: 'vertical',
+            contents: buttons,
+            paddingTop: '0px',
+          },
+        },
+      };
+
+      // 生成したFlexメッセージを返す
+      return flexMessage;
+    });
+}
 
 exports.SendMessage = (client, event) => {
     const text = event.message.text;
