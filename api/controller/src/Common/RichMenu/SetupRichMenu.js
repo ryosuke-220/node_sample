f989f672e4893efe107b689f61b58ab98944413d
ryosuke-220 <ryoske0220@gmail.com>
2025-02-23 18:27:01 +0900

    Update SetupRichMenu.js

--- api/controller/src/Common/RichMenu/SetupRichMenu.js
+++ api/controller/src/Common/RichMenu/SetupRichMenu.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const { client } = require('../../config');
 const userId = process.env.USER_ID;
-const imagePath = process.env.IMAGE_PATH;
+const imagePath = 'RichMenu.png';
 
 const richMenuData = {
     size: { width: 2500, height: 1686 },
